4ca675d7899e9b1707e1c99bd708c51eedd4badf
sohamingle <sohamingle@gmail.com>
2023-08-12 15:04:39 +0530

    change

--- server/index.js
+++ server/index.js
@@ -33,6 +33,7 @@ app.post("/api",(req,res)=>{
 
 app.get("/download",(req,res)=>{
     
+    fs.unlinkSync("output.docx")
     const filePath = path.join(__dirname,'/data')
     
     
@@ -75,10 +76,6 @@ app.get("/download",(req,res)=>{
     setTimeout(()=>{
         res.download("output.docx")
     },10000)
-    setTimeout(()=>{
-        fs.unlinkSync("output.docx")
-
-    },15000)
     
 })
 
