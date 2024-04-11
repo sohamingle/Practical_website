d82b352fa3b2a99ece8bf129faf08484415a05f9
sohamingle <sohamingle@gmail.com>
2024-04-11 16:38:47 +0530

    analytics

--- practical_website/app/layout.tsx
+++ practical_website/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import UIProvider from '@/providers/nextui-provider'
 import { Toaster } from 'react-hot-toast'
+import GoogleAnalyticsProvider from '@/providers/GoogleAnalyticsProvider'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +20,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+        <Script id="google-analytics">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <UIProvider>
-          <Toaster/>
+          <Toaster />
           {children}
         </UIProvider>
       </body>
