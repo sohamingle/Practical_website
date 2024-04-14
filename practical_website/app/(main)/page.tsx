5ff58415e91218bb98a8d146544ecb2be63d60cf
sohamingle <sohamingle@gmail.com>
2024-04-14 18:48:19 +0530

    latest first

--- practical_website/app/(main)/page.tsx
+++ practical_website/app/(main)/page.tsx
@@ -7,7 +7,7 @@ function Home() {
     <div className="p-5">
       <div className="text-xl font-bold mb-5">Select Practical</div>
       <div className='grid gap-3 md:grid-cols-2'>
-        {subjects_data.map(subject => (
+        {subjects_data.reverse().map(subject => (
           <SubjectCard key={subject.subjectName} subjectName={subject.subjectName} subjectLogo={subject.subjectLogo} link={subject.link} subjectClass={subject.subjectClass}/>
         ))}
       </div>
